0820a7abfab41e2f1688a0fa798391a3c1207c08
SrTortugardo <l4zershap3s@gmail.com>
2026-03-16 14:56:20 -0600

    Integrate keyboard and shell support in kernel
    Added keyboard and shell functionality to the kernel.

--- kernel.c
+++ kernel.c
@@ -1,6 +1,8 @@
 /* kernel.c - main kernel entry for Prix OS
  * Writes colored characters to VGA text buffer at 0xB8000
  */
+#include "drivers/keyboard.h"
+#include "shell/shell.h"
 
 typedef unsigned char  u8;
 typedef unsigned int   u32;
@@ -9,8 +11,27 @@ static u32 cursor = 0;
 
 void putchar_col(char c, u8 color) {
     volatile u8 *video = (volatile u8*)0xB8000;
+
+    if (c == '\n')
+    {
+        cursor += (160 - (cursor % 160));
+        return;
+    }
     video[cursor++] = (u8)c;
     video[cursor++] = color;
+}
+
+void clear_screen()
+{
+    volatile unsigned char *video = (volatile unsigned char*)0xB8000;
+
+    for (int i = 0; i < 80 * 25; i++)
+    {
+        video[i * 2] = ' ';
+        video[i * 2 + 1] = 0x07;
+    }
+
+    cursor = 0;
 }
 
 void kmain(void) {
@@ -21,6 +42,12 @@ void kmain(void) {
     putchar_col(' ', 0x07);
     putchar_col('O', 0x02);
     putchar_col('S', 0x02);
+    
+    //while (1)
+    //{
+    //    char c = keyboard_getchar();
+    //    putchar_col(c, 0x07);
+    //}
 
-    for (;;) { __asm__ __volatile__("hlt"); }
+    shell_run();
 }
