bc6cdcdc6706c30dff179bf702852a2c02cf3cbf
SrTortugardo <l4zershap3s@gmail.com>
2026-03-17 10:26:11 -0600

    Move functions to libc
    Removed old cursor handling and screen printing functions.

--- kernel.c
+++ kernel.c
@@ -4,49 +4,13 @@
 #include "drivers/keyboard.h"
 #include "shell/shell.h"
 #include "fs/fs.h"
+#include "kernel.h"
+#include "libc/libc.h"
 
-typedef unsigned char  u8;
-typedef unsigned int   u32;
-
-static u32 cursor = 0;
-
-void
-putchar_col(char c, u8 color) {
-    volatile u8 *video = (volatile u8*)0xB8000;
-
-    if (c == '\n')
-    {
-        cursor += (160 - (cursor % 160));
-        return;
-    }
-    video[cursor++] = (u8)c;
-    video[cursor++] = color;
-}
-
-void
-clear_screen()
-{
-    volatile unsigned char *video = (volatile unsigned char*)0xB8000;
-
-    for (int i = 0; i < 80 * 25; i++)
-    {
-        video[i * 2] = ' ';
-        video[i * 2 + 1] = 0x07;
-    }
-
-    cursor = 0;
-}
-
-void
-print(const char *str)
-{
-    for (int i = 0; str[i]; i++)
-    {
-        putchar_col(str[i], 0x07);
-    }
-}
+u32 cursor = 0;
 
 void kmain(void) {
+    clear_screen();
     putchar_col('\n', 0x01);
     putchar_col('P', 0x04);  /* red */
     putchar_col('R', 0x07);  /* light gray */
