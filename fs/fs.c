6c4577555113aa6642165d35a45671a905ef4898
SrTortugardo <l4zershap3s@gmail.com>
2026-03-16 19:46:17 -0600

    Implement basic file system functions

---
+++ fs/fs.c
@@ -0,0 +1,89 @@
+#include "fs.h"
+#include "../kernel.h"
+
+static File fs[MAX_FILES];
+static int file_count = 0;
+
+static int
+strcmp_fs(const char* a, const char* b)
+{
+    while (*a && *b)
+    {
+        if (*a != *b)
+        {
+            return 1;
+        }
+        a++;
+        b++;
+    }
+    return (*a != *b);
+}
+
+static void print_fs(const char* str) {
+    for (int i = 0; str[i]; i++)
+        putchar_col(str[i], 0x07);
+}
+
+void fs_init() {
+    for (int i = 0; i < MAX_FILES; i++) {
+        fs[i].name[0] = 0;
+        fs[i].size = 0;
+    }
+    file_count = 0;
+}
+
+
+int
+fs_create(const char* name, const uint8_t *data, uint32_t size)
+{
+    if (file_count >= MAX_FILES || size > BLOCK_SIZE)
+    {
+        return -1; // Theres no space on disk
+    }
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        int j;
+        for (j = 0; j < MAX_FILE_NAME-1 && name[j]; j++)
+        {
+            fs[i].name[j] = name[j];
+        }
+        fs[i].name[j] = 0;
+
+        for (j = 0; j < size; j++)
+        {
+            fs[i].data[j] = data[j];
+        }
+        fs[i].size = size;
+
+        file_count++;
+        return 0;
+    }
+    return -1; // theres no free slot
+}
+
+File*
+fs_get(const char* name)
+{
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        if (fs[i].name[0] && strcmp_fs(fs[i].name, name) == 0)
+        {
+            return &fs[i];
+        }
+    }
+    return 0; // not found
+}
+
+void
+fs_list()
+{
+    print_fs("Files on FS:\n");
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        if (fs[i].name[0])
+        {
+            print(fs[i].name);
+            print_fs("\n");
+        }
+    }
+}
