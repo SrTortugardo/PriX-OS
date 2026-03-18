e8118d38215932fc7a495d394497ad94f5fe2ef4
SrTortugardo <l4zershap3s@gmail.com>
2026-03-17 21:00:10 -0600

    Add scroll and check_scroll function declarations

--- libc/libc.h
+++ libc/libc.h
@@ -8,6 +8,7 @@
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
 #define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT * 2)
+#define VGA_MEMORY (u16*)0xB8000
 
 void putchar_col(char c, u8 color);
 void clear_screen();
@@ -15,6 +16,7 @@ void print(const char *str);
 void backspace();
 int strcmp(const char *a, const char *b);
 int strncmp(const char *a, const char *b, int n);
-
+void scroll();
+void check_scroll();
 
 #endif
